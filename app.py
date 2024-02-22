e56cc5e689c09fbd0b0a12e1f16ebbcb61406ec9
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2024-02-22 18:21:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from vllm import LLM, SamplingParams
+from huggingface_hub import snapshot_download
+import os
 
 class InferlessPythonModel:
     def initialize(self):
-
+        repo_id = "google/gemma-7b"
+        model_store = f"/var/nfs-mount/common_llm/{repo_id}"
+        os.makedirs(f"/var/nfs-mount/common_llm/{repo_id}", exist_ok=True)
+        
+        snapshot_download(
+                    repo_id,
+                    local_dir=model_store,
+                    token="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",
+                    ignore_patterns=["*.gguf"])
         self.sampling_params = SamplingParams(temperature=0.7, top_p=0.95,max_tokens=256)
-        self.llm = LLM(model="TheBloke/CodeLlama-70B-Python-GPTQ", quantization="gptq", dtype="float16")
+        self.llm = LLM(model=model_store,gpu_memory_utilization=0.9)
 
     def infer(self, inputs):
         prompts = inputs["prompt"]
         result = self.llm.generate(prompts, self.sampling_params)
         result_output = [output.outputs[0].text for output in result]
 
-        return {'result': result_output[0]}
+        return {'gresult': result_output[0]}
 
     def finalize(self):
         pass
